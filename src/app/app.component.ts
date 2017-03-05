81ea1c9fcaa2954603ba42eac0ba8666e31a3fb6
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-05 15:27:48 +0300

    7.15 Внесли изменения в базовый компонент app.component, чтобы он использовал и отображал компонент store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="bg-success p-a-1 text-xs-center">
-      This is SportsStore
-    </div>`,
+  template: '<app-store></app-store>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
