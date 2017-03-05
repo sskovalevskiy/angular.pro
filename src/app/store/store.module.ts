a70faed6afd61bb4038a8561b4b17ff271cbd731
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-05 16:16:48 +0300

    7.23 Регистрируем директиву CounterDirective в store.module.ts

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,14 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ModelModule} from '../model/model.module';
 import {NgModule} from '@angular/core';
 import {CounterDirective} from './counter.directive';
-import { CartSummaryComponent } from './cart-summary/cart-summary.component';
-import { CartDetailComponent } from './cart-detail/cart-detail.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import {CartSummaryComponent} from './cart-summary/cart-summary.component';
+import {CartDetailComponent} from './cart-detail/cart-detail.component';
+import {CheckoutComponent} from './checkout/checkout.component';
 import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent],
+  declarations: [StoreComponent, CounterDirective],
   exports: [StoreComponent]
 })
-export class StoreModule { }
+export class StoreModule {
+}
