1a3c3c911017e7a3c7f8746c4277f75b42363ace
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-05 14:43:16 +0300

    7.6 Добавили файл app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {StoreComponent} from './store/store.component';
+import {StoreModule} from './store/store.module';
+import {RouterModule} from '@angular/router';
+import {CartDetailComponent} from './store/cart-detail/cart-detail.component';
+import {CheckoutComponent} from './store/checkout/checkout.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
