7eebdcf1b1ef9d9d7cd0e170ab802ce1ffd8634a
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-05 14:34:44 +0300

    7.5 Добавил шаблон разметки в app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="bg-success p-a-1 text-xs-center">
+      This is SportsStore
+    </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
