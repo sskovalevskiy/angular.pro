2fb0e56dc543b9e2bc1cb6daa0e4f4f5c891eff1
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-10 18:32:04 +0300

    8.18 Добавляем в конструктор компонента CartDetailComponent параметр Cart, который будет доступен в шаблоне через свойство cart

--- src/app/store/cart-detail/cart-detail.component.ts
+++ src/app/store/cart-detail/cart-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Cart} from '../../model/cart.model';
 
 @Component({
   selector: 'app-cart-detail',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cart: Cart) {
+  }
 
   ngOnInit() {
   }
