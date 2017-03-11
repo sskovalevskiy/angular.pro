25d8a706a0536414516ee279f3039c629d81c98a
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-11 19:10:06 +0300

    8.24 Редактируем компонент CheckoutComponent для работы с формой оформления заказа

--- src/app/store/checkout/checkout.component.ts
+++ src/app/store/checkout/checkout.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {OrderRepository} from '../../model/order.repository';
+import {Order} from '../../model/order.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent {
 
-  constructor() { }
+  orderSent = false;
+  submitted = false;
 
-  ngOnInit() {
+  constructor(public repository: OrderRepository,
+              public order: Order) {
   }
 
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.repository.saveOrder(this.order).subscribe(order => {
+        this.order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
 }
