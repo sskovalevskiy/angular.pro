2e029ae938dca6ea468e13814ec7b276680eaca3
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-12 14:11:51 +0300

    8.28 Изменяем конфигурацию службы в файле model.module.ts для ипользования RestDataSource в качестве поставщика данных

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,9 +4,13 @@ import {StaticDataSource} from './static.datasource';
 import {Cart} from './cart.model';
 import {OrderRepository} from './order.repository';
 import {Order} from './order.model';
+import {RestDataSource} from './rest.datasource';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+  imports: [HttpModule],
+  providers: [ProductRepository, Cart, Order, OrderRepository,
+    {provide: StaticDataSource, useClass: RestDataSource}]
 })
 export class ModelModule {
 }
