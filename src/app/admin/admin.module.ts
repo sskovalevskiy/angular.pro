3c4dafc3670dcf0319113a308d637ac6d510a8a5
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-13 09:12:39 +0300

    9.13 Защищаем путь /main в файле admin.module.ts с помощью AuthGuard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,15 +4,17 @@ import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {AuthGuard} from './auth.guard';
 
 const routing = RouterModule.forChild([
   {path: 'auth', component: AuthComponent},
-  {path: 'main', component: AdminComponent},
+  {path: 'main', component: AdminComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: 'auth'}
 ]);
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing],
+  providers: [AuthGuard],
   declarations: [AuthComponent, AdminComponent]
 })
 export class AdminModule {
