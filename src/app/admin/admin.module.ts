ef368e0e0f7b341d818a6f2589c803394ded869b
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-14 16:06:27 +0300

    9.22 Расширяем конфигурацию маршрутизации в admin.module.ts, подключаем заглушки ProductTableComponent, ProductEditorComponent, OrderTableComponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,17 +5,29 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {AuthGuard} from './auth.guard';
+import {OrderTableComponent} from './orderTable.component';
+import {ProductTableComponent} from './productTable.component';
+import {ProductEditorComponent} from './productEditor.component';
 
 const routing = RouterModule.forChild([
   {path: 'auth', component: AuthComponent},
-  {path: 'main', component: AdminComponent, canActivate: [AuthGuard]},
+  {
+    path: 'main', component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      {path: 'products/:mode/:id', component: ProductEditorComponent},
+      {path: 'products/:mode', component: ProductEditorComponent},
+      {path: 'products', component: ProductTableComponent},
+      {path: 'orders', component: OrderTableComponent},
+      {path: '**', redirectTo: 'products'}
+    ]
+  },
   {path: '**', redirectTo: 'auth'}
 ]);
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing],
   providers: [AuthGuard],
-  declarations: [AuthComponent, AdminComponent]
+  declarations: [AuthComponent, AdminComponent, ProductTableComponent, ProductEditorComponent, OrderTableComponent]
 })
 export class AdminModule {
 }
