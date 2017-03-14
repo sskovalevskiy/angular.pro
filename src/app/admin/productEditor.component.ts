12cff38c77b275c1c5ea1bcfc6cd6e8e4715f4df
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-14 16:25:51 +0300

    9.25 Меняем заглушку ProductEditorComponent на полноценный код создания и редактирования товара

--- src/app/admin/productEditor.component.ts
+++ src/app/admin/productEditor.component.ts
@@ -1,10 +1,28 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ProductRepository} from '../model/product.repository';
+import {Product} from '../model/product.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  template: `
-    <div class="bg-warning p-a-1">
-      <h3>Product Editor Placeholder</h3>
-    </div>`
+  templateUrl: 'productEditor.component.html'
 })
 export class ProductEditorComponent {
+  editing = false;
+  product: Product = new Product();
+
+  constructor(private repository: ProductRepository,
+              private router: Router,
+              activeRoute: ActivatedRoute) {
+    this.editing = activeRoute.snapshot.params['mode'] == 'edit';
+    if (this.editing) {
+      Object.assign(this.product,
+        repository.getProduct(activeRoute.snapshot.params['id']));
+    }
+  }
+
+  save(form: NgForm) {
+    this.repository.saveProduct(this.product);
+    this.router.navigateByUrl('/admin/main/products');
+  }
 }
