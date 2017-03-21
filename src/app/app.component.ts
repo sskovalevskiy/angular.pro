2f93388b64127ab97b146bd3ccb8b512e9e02fce
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-21 10:05:49 +0300

    14.4 Удаляем ненужные методы класса AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ApplicationRef, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {Model} from './repository.model';
 import {Product} from './product.model';
 
@@ -10,30 +10,11 @@ import {Product} from './product.model';
 export class AppComponent {
   model: Model = new Model();
 
-  targetName: string = 'Kayak';
-
-  constructor(ref: ApplicationRef) {
-    (<any>window).appRef = ref;
-    (<any>window).model = this.model;
-  }
-
-  getProductByPosition(position: number): Product {
-    return this.model.getProducts()[position];
-  }
-
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
 
   getProducts(): Product[] {
     return this.model.getProducts();
-  }
-
-  getProductCount(): number {
-    return this.getProducts().length;
-  }
-
-  getKey(index: number, product: Product) {
-    return product.id;
   }
 }
