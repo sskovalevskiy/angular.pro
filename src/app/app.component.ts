6f087aebb7f97e7e1d2a34a494b0f33bac1e37a9
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2017-03-23 13:27:15 +0300

    14.18 Добавляем метод getValidationMessages() возвращающий массив сообщений об ошибках

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,4 +26,25 @@ export class AppComponent {
   addProduct(p: Product) {
     console.log('New Product: ' + this.jsonProduct);
   }
+
+  getValidationMessages(state: any, thingName?: string) {
+    let thing: string = state.path || thingName;
+    let messages: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case 'required':
+            messages.push(`You must enter a ${thing}`);
+            break;
+          case 'minLength':
+            messages.push(`A ${thing} must be at least ${state.errors['minLength'].requiredLength} characters`);
+            break;
+          case 'pattern':
+            messages.push(`The ${thing} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
 }
